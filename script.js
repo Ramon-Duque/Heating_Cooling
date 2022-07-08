4496dbd49491602c583f4cfef72ceab3d96ae735
Ramon Duque <ramonduke@gmail.com>
2022-07-08 14:32:52 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let actualTemp = 68;
+let desiredTemp = 72;
+
+if (actualTemp < desiredTemp) {
+    console.log("Run heat");
+} else if (actualTemp === desiredTemp){
+    console.log("Standby");
+} else {
+    console.log("Run A/C");
+}
+
